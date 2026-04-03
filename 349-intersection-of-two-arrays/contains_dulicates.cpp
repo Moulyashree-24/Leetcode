80c1f64e2cb71ae1678ca5cb68b4587487c61703
moulyashree k <MOULYASHREE.20241CSD0126@presidencyuniversity.in>
2026-04-03 07:13:04 +0530

    Update contains_dulicates.cpp

--- 349-intersection-of-two-arrays/contains_dulicates.cpp
+++ 349-intersection-of-two-arrays/contains_dulicates.cpp
@@ -1,12 +1,1 @@
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-       sort(nums.begin(), nums.end()); // sorts the array so we can easily identify duplicates
-    for(int i=0; i<nums.size()-1; i++)
-       if(nums[i]==nums[i+1])
-       {
-        return true;
-       }
-    return false;
-    }
-};
+
