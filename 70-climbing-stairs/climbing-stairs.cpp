aad6569a31ad8cf5341cda1d8b9ad4855c6eac0b
moulyashree k <MOULYASHREE.20241CSD0126@presidencyuniversity.in>
2026-06-16 08:19:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.73%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+        {
+            return n;
+        }
+        int a=1,b=2,c;
+        for(int i=3; i<=n; i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+    return c;
+    }
+};
+ //  if(n==0||n==1)
+    //  {
+    //     return 1;
+    //  }
+    //  else
+    //  {
+    //     return climbStairs(n-1)+climbStairs(n-2);
+    //  }
