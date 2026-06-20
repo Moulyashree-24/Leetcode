f1bd921a22c5b76f4bb9f5253f91ebc91a018a3b
moulyashree k <MOULYASHREE.20241CSD0126@presidencyuniversity.in>
2026-06-20 16:22:23 +0530

    Time: 4 ms (39.16%) | Memory: 30.4 MB (39.95%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i=0; i<nums.size(); i++)
+        int j=nums.size()-1,k=0;
+        vector<int> ans(j+1);
+        for(int i=nums.size()-1; i>=0; i--)
         {
-            nums[i]=nums[i]*nums[i];
+            if(abs(nums[k])>abs(nums[j]))
+            {
+                ans[i]=nums[k]*nums[k];
+                k++;
+            }
+            else
+            {
+               ans[i]=nums[j]*nums[j];
+                j--;
+            }
         }
-        sort(nums.begin(),nums.end());
-     return nums;
+     return ans;
+
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             nums[i]=nums[i]*nums[i];
+//         }
+//         sort(nums.begin(),nums.end());
+//      return nums;
+//     }
+// };
